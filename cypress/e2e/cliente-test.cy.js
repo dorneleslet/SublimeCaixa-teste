93d0b8822eda3b510f47a1bdebbaedc92b32a764
dorneleslet <dornelesleticia1@gmail.com>
2026-01-22 09:38:38 +0000

    alterações com data-test  em caixa.html

--- cypress/e2e/cliente-test.cy.js
+++ cypress/e2e/cliente-test.cy.js
@@ -15,7 +15,7 @@ describe('Cadastrar novo cliente', () => {
   })
   it('Deve carregar a página de clientes, preencher os campos e cadastrar o cliente com sucesso', () => {
     cy.visit('https://sublimecaixa-teste.onrender.com/clientes/');
-    cy.cadastro('Jane Doe', '555874965', '555874965', '11/11/1985', 'jane.doe@email.com.br')
+    cy.cadastro('Jane Doe', '555874965', '512374965', '11/11/1985', 'jane.doe@email.com.br')
     // cy.get('#nome').type('Cliente Teste');
     // cy.get('#telefone').type('123456789');
     // cy.get('#nif').type('555222126');
@@ -32,6 +32,18 @@ describe('Cadastrar novo cliente', () => {
     cy.get('[data-test="preco-cadastro"]').type('45');
     cy.get('[data-test="botao-cadastro"]').click();
     cy.contains('Serviço cadastrado com sucesso!').should('be.visible');
-  }
-)
+  })
+
+  it('Acessar a página de caixa e realizar uma venda com sucesso', () => {
+    cy.visit('https://sublimecaixa-teste.onrender.com/caixa/');
+    cy.get('[data-test="buscar_cliente"]').type('Leticia Dorneles').click();
+    cy.get('[data-test="buscar_servico"]').type('Serviço Teste').click();
+    cy.get('[data-test="profissional"]').type('Joana');
+    cy.get('[data-test="desconto"]').type('10');
+    cy.get('[data-test="sinal"]').type('10');
+    cy.get('[data-test="fatura"]').type('Sim').click();
+    cy.get('[data-test="pagamento"]').type('Dinheiro').click();
+    cy.get('[data-test="finalizar-venda"]').click();
+    cy.contains('Venda registrada com sucesso!').should('be.visible');
+  })
 })
