356b9eb42710a1ee85afec95d545074c5a2df0a2
dorneleslet <dornelesleticia1@gmail.com>
2026-02-23 14:20:24 +0000

    Modificando configuração do banco de dados para rodar localmente

--- sublimecaixa/settings.py
+++ sublimecaixa/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-w0pr7xt=t^_o+@b42op_k4@99l#gm1jrogynvc4f!f(^%ri+ff
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["sublimecaixa-teste.onrender.com", "www.sublimecaixa-teste.onrender.com"]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'sublimecaixa.wsgi.application'
 
 DATABASES = {
     'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'testesublime_db',
-    'USER': 'testesublime_db_user',
-    'PASSWORD': 'qAAhscvxo88dj8vTZRMxaEuBbDKd7FCN',
-    'HOST': 'dpg-d5mij3mmcj7s73c43j8g-a.virginia-postgres.render.com',
-    'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
