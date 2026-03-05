d2a87e2a2597e21d46377dc1ffb762f013a75c2a
dorneleslet <dornelesleticia1@gmail.com>
2026-03-05 18:48:22 +0000

    Adicionando novos testes automatizados na página serviços

--- cypress/e2e/cadastro-massa.cy.js
+++ cypress/e2e/cadastro-massa.cy.js
@@ -5,16 +5,18 @@ describe('Página de cadastro de clientes', () => {
         cy.visit('http://127.0.0.1:8000/clientes/');
     })
 
-    it('Deve preencher os campos do cadastro de clientes corretamente.', () => {
-        const cliente = gerarClientes();
+    it('Deve cadastrar 20 clientes corretamente.', () => {
+        for (let i = 0; i < 20; i++) { // Alterar aqui a quantidade de cadastro
+            const cliente = gerarClientes();
 
-        cy.get('[data-test="nome-cadastro"]').clear().type(cliente.nome);
-        cy.get('[data-test="telefone-cadastro"]').clear().type(cliente.telefone);
-        cy.get('[data-test="nif-cadastro"]').clear().type(cliente.nif);
-        cy.get('[data-test="nascimento-cadastro"]').clear().type(cliente.nascimento);
-        cy.get('[data-test="email-cadastro"]').clear().type(cliente.email);
+            cy.get('[data-test="nome-cadastro"]').clear().type(cliente.nome);
+            cy.get('[data-test="telefone-cadastro"]').clear().type(cliente.telefone);
+            cy.get('[data-test="nif-cadastro"]').clear().type(cliente.nif);
+            cy.get('[data-test="nascimento-cadastro"]').clear().type(cliente.nascimento);
+            cy.get('[data-test="email-cadastro"]').clear().type(cliente.email);
 
-        cy.get('[data-test="btn-salvar"]').click();
-        cy.contains('Cliente cadastrado com sucesso!').should('be.visible');
+            cy.get('[data-test="btn-salvar"]').click();
+            cy.contains('Cliente cadastrado com sucesso!').should('be.visible');
+        }
     })
 })    
